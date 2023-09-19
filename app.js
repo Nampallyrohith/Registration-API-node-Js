508734dbb25e574b2209ad650516591606922eff
Rohith Nampally <rohithnampelly57@gmail.com>
2023-09-19 09:32:23 +0000

    added login and changed password

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ initializeDBServer();
 //API 1
 // POST
 
-app.post("/register/", async (request, response) => {
+app.post("/register", async (request, response) => {
     const {username, name, password, gender, location} = request.body;
     let hashedPassword = await bcrypt.hash(password, 10);
     const selectedQuery = `SELECT * FROM user WHERE username = '${username}'`;
@@ -62,5 +62,56 @@ app.post("/register/", async (request, response) => {
     }
 });
 
+app.post("/login", async (request, response) => {
+    const {username, password} = request.body;
+    const selectedQuery = `SELECT * FROM user WHERE username = '${username}'`;
+
+    const query = await db.get(selectedQuery);
+    console.log(query);
+    if (username === undefined) {
+        response.status(400);
+        response.send("Invalid user");
+    }
+    else{
+        const checkPassword = await bcrypt.compare(password, query.password);
+        if (checkPassword === true) {
+            response.status(200);
+            response.send("Login success!")
+        } else{
+            response.status(400);
+            response.send("Invalid password");
+        }
+    }
+})
+
+//API 3
+// Change password
+
+app.put("/change-password", async (request, response) => {
+    const {username, oldPassword, newPassword} = request.body;
+    const selectedQuery = `SELECT * FROM user WHERE username = '${username}'`;
+
+    const query = await db.get(selectedQuery);
+    const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+    const checkOldPassword = await bcrypt.compare(oldPassword, query.password);
+    if (checkOldPassword === false) {
+        response.status(400)
+        response.send("Invalid current password")
+    } else{
+        if (newPassword.length < 5) {
+            response.status(400);
+            response.send("Password is too short");
+        } else{
+            const newPasswordAdding = `
+            UPDATE user 
+            SET
+            password = '${hashedNewPassword}'
+            `;
+            const updatedPassword = await db.run(newPasswordAdding);
+            response.status(200);
+            response.send("Password updated");
+        }
+    }
+})
 
 module.exports = app;
